822196fc01e2debf0c54769d42b2a1507adc4aab
javierR26 <javiecardenas128@gmail.com>
2026-01-09 20:14:20 -0500

    commit ocho agrege un nuevo proyecto de la galeria

--- data/projects.ts
+++ data/projects.ts
@@ -1,38 +1,47 @@
 export const projects = [
-        {
+    {
         id: 1,
         title: "Venta de artesanias",
-        img:"/img/Manart.jpeg",
+        img: "/img/Manart.jpeg",
         description: "e-comercer de venta de artesanias",
-        technologies: ["Next.js", "Spring Boot","Spring Boot Security", "MySQL", "Tailwind CSS"],
+        technologies: ["Next.js", "Spring Boot", "Spring Boot Security", "MySQL", "Tailwind CSS"],
         gitHub: "https://github.com/episteme-debug/project-manart-backend",
         demo: ""
     },
     {
         id: 2,
         title: "to do",
-        img:"/img/to_do.png",
+        img: "/img/to_do.png",
         description: "Es un Crud de tareas simple",
         technologies: ["Html", "JavaScript", "CSS", "Bootstrap"],
         gitHub: "https://github.com/javierR26/to-do",
         demo: "https://luminous-valkyrie-0fa8cc.netlify.app/"
     },
-        {
+    {
         id: 3,
         title: "Calculadora",
-        img:"/img/Calculadora.png",
+        img: "/img/Calculadora.png",
         description: "Calculadora simple hecha con JavaScript",
         technologies: ["Html", "JavaScript", "CSS", "Bootstrap"],
         gitHub: "https://github.com/javierR26/Calculadora",
         demo: "https://hilarious-tapioca-3438d0.netlify.app/"
     },
-        {
+    {
         id: 4,
         title: "Pagina del Clima",
-        img:"/img/Clima.png",
+        img: "/img/Clima.png",
         description: "Aplicación que muestra el clima actual usando una API",
         technologies: ["Html", "JavaScript", "CSS", "Bootstrap"],
         gitHub: "https://github.com/javierR26/Clima",
         demo: "https://thunderous-strudel-426bf8.netlify.app/"
+    },
+    {
+        id: 5,
+        title: "Pagina galieria",
+        img: "/img/Galeria.png",
+        description: "Aplicación de una galeria y permite agregar a favoritos usando una API",
+        technologies: ["Next.js", "React", "JavaScript / TypeScript", "Node.js", "Fetch API", "Tailwind"],
+        gitHub: "https://github.com/javierR26/Galeria",
+        demo: ""
     },
 ];
